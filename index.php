c2e9f2c7d484613a0126fcdf2d2221a39ed73129
Jessica <jesscara.993@gmail.com>
2023-01-25 17:17:15 +0100

    class product, methods, test vardump

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+
+class Product
+{
+  private $name;
+  private $type;
+  private $category;
+  private $description;
+  private $price;
+
+  function __construct($name, $type, $category, $description, $price)
+  {
+    $this->setName($name);
+    $this->setType($type);
+    $this->setCategory($category);
+    $this->setDescription($description);
+    $this->setPrice($price);
+  }
+
+  // metodo per nome
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  // metodo per tipologia
+  public function setType($type)
+  {
+    $this->type = $type;
+  }
+
+  public function getType()
+  {
+    return $this->type;
+  }
+
+  // metodo per categoria
+  public function setCategory($category)
+  {
+    $this->category = $category;
+  }
+
+  public function getCategory()
+  {
+    return $this->category;
+  }
+
+  // metodo per descrizione
+  public function setDescription($description)
+  {
+    $this->description = $description;
+  }
+
+  public function getDescription()
+  {
+    return $this->description;
+  }
+
+  // metodo per prezzo
+  public function setPrice($price)
+  {
+    $this->price = $price;
+  }
+
+  public function getPrice()
+  {
+    return $this->price;
+  }
+}
+
+// prodotto random per prova stampa in pagina
+$products = new Product('palla', 'palla in gomma', 'cani', 'safnjsdngjdfngbjfnb', '20$');
+// controllo funzionalità
+var_dump($products);
